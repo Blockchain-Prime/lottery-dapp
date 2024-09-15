eabdcb994f89f027f4be3317e5c6f11cffbc698d
Raebeatrose <rasbinbot@gmail.com>
2024-09-15 12:09:49 +0530

    feat: drop

---
+++ assets/ContentWrapper-a8302305.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-72c0ec7f.js";const n=({children:e})=>r("div",{className:"content-wrapper",children:r("div",{className:"content-wrapper__container",children:e})});export{n as default};
